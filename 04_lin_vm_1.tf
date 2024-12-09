bde493d0cc56f9f79905dfeed06c22c739c7e746
SUD0x0 <54279221+sud0x0@users.noreply.github.com>
2024-12-09 22:16:42 +0800

    Update 04_lin_vm_1.tf

--- 04_lin_vm_1.tf
+++ 04_lin_vm_1.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "Server-Lin-1_ip" {
   name                = join("-", [local.project_name, "Server-Lin-1-IP"])
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   tags                = local.common_tags
 
   timeouts {
